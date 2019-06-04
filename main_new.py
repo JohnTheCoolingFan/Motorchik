3182171d84783a106582f68c7bcfa4e33a619b7e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-06-05 00:50:19 +0300

    Removed config checks

--- main_new.py
+++ main_new.py
@@ -36,7 +36,7 @@ async def on_message(message):
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CheckFailure):
-        ctx.send(content = 'This command is disbaled in this channle or on this server.')
+        ctx.send('This command is disbaled in this channle or on this server.', delete_after = 5)
 
 @bot.event
 async def on_member_join(member):
@@ -52,38 +52,23 @@ async def on_member_remove(member):
 
 @bot.command(hidden = True, help = 'Returns text typed after $test')
 async def test(ctx, arg):
-    if bot_config[str(ctx.guild.id)]['commands']['test']['enabled']:
-        await ctx.send(arg)
-    else:
-        await ctx.send('This command is disabled in this channel', delete_after = 5)
+    await ctx.send(arg)
 
 @bot.command(description = '\"Hello\" in English', brief = '\"Hello\" in English', help = 'Returns \"Hello\" in English')
 async def hello(ctx):
-    if bot_config[str(ctx.guild.id)]['commands']['hello']['enabled']:
-        await ctx.send('Hello!')
-    else:
-        await ctx.send('This command is disabled in this channel', delete_after = 5)
+    await ctx.send('Hello!')
 
 @bot.command(aliases = ['gutentag'], description = '\"Hello\" in German', brief = '\"Hello\" in German', help = 'Returns \"Hello\" in German')
 async def hello_german(ctx):
-    if bot_config[str(ctx.guild.id)]['commands']['hello_german']['enabled']:
-        await ctx.send('Guten tag')
-    else:
-        await ctx.send('This command is disabled in this channel', delete_after = 5)
+    await ctx.send('Guten tag')
 
 @bot.command(aliases = ['privet'], description = '\"Hello\" in Russian', brief = '\"Hello\" in Russian', help = 'Returns \"Hello\" in Russian')
 async def hello_russian(ctx):
-    if bot_config[str(ctx.guild.id)]['commands']['hello_russian']['enabled']:
-        await ctx.send('Приветствую!')
-    else:
-        await ctx.send('This command is disabled in this channel', delete_after = 5)
+    await ctx.send('Приветствую!')
 
 @bot.command(hidden = True, help = 'tag')
 async def guten(ctx):
-    if bot_config[str(ctx.guild.id)]['commands']['guten']['enabled']:
-        await ctx.send('tag')
-    else:
-        await ctx.send('This command is disabled in this channel', delete_after = 5)
+    await ctx.send('tag')
 
 @bot.command(hidden = True, help = 'You spin me right round, baby, right round')
 async def spin(ctx):
@@ -91,39 +76,27 @@ async def spin(ctx):
 
 @bot.command(hidden = True, aliases = ['XcQ'], help = 'You\'ve got RICKROLLED, LUL')
 async def rickroll(ctx):
-    if bot_config[str(ctx.guild.id)]['commands']['rickroll']['enabled']:
-        await ctx.send('<https://www.youtube.com/watch?v=dQw4w9WgXcQ>')
-        await ctx.send('<:kappa_jtcf:546748910765604875>')
-    else:
-        await ctx.send('This command is disabled in this channel', delete_after = 5)
+    await ctx.send('<https://www.youtube.com/watch?v=dQw4w9WgXcQ>')
+    await ctx.send('<:kappa_jtcf:546748910765604875>')
 
 @bot.command(aliases = ['modstat'], description = 'Info about mods', brief = 'Info about mods', help = 'Prints a bunch of commands for uBot to display info about mods')
 async def mods_statistics(ctx):
-    if bot_config[str(ctx.guild.id)]['commands']['mods_statistics']['enabled']:
-        if ctx.guild.id == TEST_SERVER_ID or ctx.channel.id == JTCF_MODSTAT_CHANNEL or ctx.channel.id == JTCF_BOTFLOOD_CHANNEL:
-            for modname in MOD_LIST:
-                await ctx.send(content = '>>{0}<<'.format(modname), delete_after = 1)
-            await ctx.message.delete()
-        else:
-            await ctx.send(content = '{0.author.mention}, This command cannot be used in this channel'.format(ctx), delete_after = 5)
+    if ctx.guild.id == TEST_SERVER_ID or ctx.channel.id == JTCF_MODSTAT_CHANNEL or ctx.channel.id == JTCF_BOTFLOOD_CHANNEL:
+        for modname in MOD_LIST:
+            await ctx.send(content = '>>{0}<<'.format(modname), delete_after = 1)
+        await ctx.message.delete()
     else:
-        await ctx.send('This command is disabled in this channel', delete_after = 5)
+        await ctx.send(content = '{0.author.mention}, This command cannot be used in this channel'.format(ctx), delete_after = 5)
 
 @bot.command(hidden = True, help = 'pong')
 async def ping(ctx):
-    if bot_config[str(ctx.guild.id)]['commands']['ping']['enabled']:
-        await ctx.send('pong')
-    else:
-        await ctx.send('This command is disabled in this channel', delete_after = 5)
+    await ctx.send('pong')
 
 @bot.command(aliases = ['clear'], description = 'Clear chat', brief = 'Clear chat', help = 'Deletes specified count of messages in this channel. Only for admins and moderators.')
 async def clearchat(ctx, arg: int):
-    if bot_config[str(ctx.guild.id)]['commands']['clearchat']['enabled']:
-        if ctx.author.permissions_in(ctx.channel).manage_messages:
-            async for message in ctx.channel.history(limit = arg + 1):
-                await message.delete()
-    else:
-        await ctx.send('This command is disabled in this channel', delete_after = 5)
+    if ctx.author.permissions_in(ctx.channel).manage_messages:
+        async for message in ctx.channel.history(limit = arg + 1):
+            await message.delete()
 
 @bot.group(case_sensitive = True)
 async def config(ctx):
