4dc02e166bcb7d377972666545fce8dd15f8e681
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-06-05 16:00:37 +0300

    Some code formatting for check function

--- main_new.py
+++ main_new.py
@@ -49,10 +49,10 @@ async def on_member_remove(member):
         await member.guild.get_channel(bot_config[str(member.guild.id)]['welcome_channel_id']).send('Goodbye, {0.mention}!'.format(member))
 
 async def is_enabled(ctx):
-    return                    bot_config[str(ctx.guild.id)]['commands'][ctx.command.name]['enabled']\
-    and not ctx.channel.id in bot_config[str(ctx.guild.id)]['commands'][ctx.command.name]['blacklist']\
-    and    (ctx.channel.id in bot_config[str(ctx.guild.id)]['commands'][ctx.command.name]['whitelist']\
-    or                    len(bot_config[str(ctx.guild.id)]['commands'][ctx.command.name]['whitelist']) == 0)
+    return                     bot_config[str(ctx.guild.id)]['commands'][ctx.command.name]['enabled']\
+    and  ctx.channel.id not in bot_config[str(ctx.guild.id)]['commands'][ctx.command.name]['blacklist']\
+    and (ctx.channel.id     in bot_config[str(ctx.guild.id)]['commands'][ctx.command.name]['whitelist']\
+    or                     len(bot_config[str(ctx.guild.id)]['commands'][ctx.command.name]['whitelist']) == 0)
 
 @bot.command(hidden = True, help = 'Returns text typed after $test')
 @commands.check(is_enabled)
