e07041d6a26c48be9a68f0a99d2baca28fe5132c
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-08-13 18:07:31 +0300

    Added FunCommands module

--- main.py
+++ main.py
@@ -25,26 +25,7 @@ async def on_ready():
 
 bot.load_extension('greetings')
 bot.load_extension('testcommands')
-
-class FunCommands(commands.Cog, name='Fun'):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command(help='You spin me right round, baby, right round')
-    async def spin(self, ctx):
-        await ctx.send('https://www.youtube.com/watch?v=PGNiXGX2nLU')
-
-    @commands.command(aliases=['XcQ'], help='You\'ve got RICKROLLED, LUL')
-    async def rickroll(self, ctx):
-        await ctx.send('<https://www.youtube.com/watch?v=dQw4w9WgXcQ>')
-        await ctx.send('<:kappa_jtcf:546748910765604875>')
-
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send('pong')
-
-bot.add_cog(FunCommands(bot))
-
+bot.load_extension('funcommandss')
 
 class FactorioCog(commands.Cog, name='Factorio'):
     def __init__(self, bot):
