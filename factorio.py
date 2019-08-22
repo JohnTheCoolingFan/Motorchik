674b0bfa5fc9ecb2b62441b647fa4bb66b54ea8b
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-08-22 19:49:54 +0300

    Added new_mods_statistics command

--- factorio.py
+++ factorio.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import requests as req
 MOD_LIST = ['Placeable-off-grid', 'No Artillery Map Reveal', 'Random Factorio Things', 'Plutonium Energy', 'RealisticReactors Ingo']
 
 class FactorioCog(commands.Cog, name='Factorio'):
@@ -14,6 +15,14 @@ class FactorioCog(commands.Cog, name='Factorio'):
             for modname in MOD_LIST:
                 await ctx.send(content='>>{0}<<'.format(modname), delete_after=1)
             await ctx.message.delete()
+
+    @commands.command(aliases=['nmodstat'])
+    async def new_mods_statistics(self, ctx, mod_name):
+        request = req.get('https://mods.factorio.com/api/mods/'+mod_name)
+        if request.status_code != 404:
+            await ctx.send('https://mods.factorio.com'+sorted(request.json()['releases'], key=lambda release: release['version'], reverse=True)[0]['download_url']+'\nOR\n'+'https://factorio-launcher-mods.storage.googleapis.com/'+mod_name+'/'+sorted(request.json()['releases'], key=lambda release: release['version'], reverse=True)[0]['version']+'.zip')
+        else:
+            await ctx.send('MOD NOT FOUND')
 
 def setup(bot):
     bot.add_cog(FactorioCog(bot))
