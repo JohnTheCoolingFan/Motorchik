7207cadc1b188e21fe6095bfaac85c7eb783986a
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-09-02 00:34:35 +0300

    A little improvement to clear command

--- moderation.py
+++ moderation.py
@@ -9,6 +9,7 @@ class Moderation(commands.Cog):
     @commands.command(aliases=['clear'], description='Clear chat', brief='Clear chat', help='Deletes specified count of messages in this channel. Can be used only by members with messages managing permission.')
     @commands.has_permissions(manage_messages=True)
     async def clearchat(self, ctx, messages_count: int):
+        await ctx.message.delete()
         deleted = await ctx.channel.purge(limit=messages_count)
         await ctx.send('Deleted {} message(s)'.format(len(deleted)), delete_after=3)
 
