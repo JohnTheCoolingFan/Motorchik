9fcb37a60d83c86a7e2ce2eacbda6b2b3a9d6cf8
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-10-05 23:51:23 +0300

    Added "Error Handling" extension. Not enabled yet

---
+++ errorhandling.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+class ErrorHandling(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, error: commands.CommandError):
+        print(error)
+
+def setup(bot):
+    bot.add_cog(ErrorHandling(bot))
