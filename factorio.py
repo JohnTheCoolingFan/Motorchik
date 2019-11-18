876e819238cc2b26e38abbde714163edfc0702e1
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-11-18 18:35:05 +0300

    Factorio cog refactoriong (?)

--- factorio.py
+++ factorio.py
@@ -7,63 +7,40 @@ from colorthief import ColorThief
 from io import BytesIO
 from user_config import UserConfig
 
-MOD_LIST_UBOT = ['Placeable-off-grid', 'No Artillery Map Reveal', 'Random Factorio Things', 'Plutonium Energy', 'RealisticReactors Ingo']
 MOD_LIST_MOTORCHIK = ['PlaceableOffGrid', 'NoArtilleryMapReveal', 'RandomFactorioThings', 'PlutoniumEnergy', 'RealisticReactors']
 
 # TODO: make customizable mod-list, which will update automatically over time by editing messages
-# TODO: this cog needs some reorganization...
 
 
 class FactorioCog(commands.Cog, name='Factorio'):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(aliases=['modstat'], description='Info about mods', brief='Info about mods', help='Prints a bunch of commands for uBot to display info about mods')
-    async def mods_statistics(self, ctx: commands.Context, *mod_names: str):
+    @commands.command(aliases=['nnmodstat'])
+    async def newer_mods_statistics(self, ctx: commands.Context, *mods_names):
         if UserConfig.check(ctx.author):
             userconfig = UserConfig(ctx.author)
-            userconfig.add_xp(5)
+            userconfig.add_xp(5*len(mods_names))
 
-        if mod_names:
-            for modname in mod_names:
-                await ctx.send('>>'+modname+'<<', delete_after=1)
+        mods_data = await self.get_mods_info(mods_names)
+        if mods_data:
+            for mod_data in mods_data:
+                await self.send_mod_embed(ctx, mod_data)
         else:
-            for modname in MOD_LIST_UBOT:
-                await ctx.send(content='>>{0}<<'.format(modname), delete_after=1)
-            await ctx.message.delete()
-
-    @commands.command(aliases=['nmodstat'])
-    async def new_mods_statistics(self, ctx: commands.Context, *, mod_name: str):
-        if UserConfig.check(ctx.author):
-            userconfig = UserConfig(ctx.author)
-            userconfig.add_xp(15)
-
-        request = req.get('https://mods.factorio.com/api/mods/'+mod_name)
-        if request.status_code == 200:
-            json_req = request.json()
-            latest_release = sorted(json_req['releases'], key=lambda release: release['version'], reverse=True)[0]
-            if json_req['thumbnail'] != '/assets/.thumb.png':
-                thumb_color = discord.Color.from_rgb(*ColorThief(
-                    BytesIO(req.get('https://mods-data.factorio.com' + json_req['thumbnail']).content)).get_color())
-            else:
-                thumb_color = discord.Color.from_rgb(47, 137, 197)
-            embed = discord.Embed(title=json_req['title'], description=json_req['summary'], url='https://mods.factorio.com/mod/'+mod_name, timestamp=parser.isoparse(latest_release['released_at']), color=thumb_color)
-            embed.set_footer(text='Latest update released at:')
-            if json_req['thumbnail'] != '/assets/.thumb.png':
-                embed.set_thumbnail(url='https://mods-data.factorio.com'+json_req['thumbnail'])
-            embed.add_field(name='Game Version', value=latest_release['info_json']['factorio_version'])
-            embed.add_field(name='Download', value='[From Official Mod Portal](https://mods.factorio.com{download_url})\n[From Factorio Launcher storage](https://factorio-launcher-mods.storage.googleapis.com/{mod_name}/{version}.zip)'.format(download_url=latest_release['download_url'], mod_name=mod_name, version=latest_release['version']))
-            embed.add_field(name='Latest Version', value=latest_release['version'])
-            embed.add_field(name='Downloaded', value=str(json_req['downloads_count'])+' times')
-            embed.add_field(name='Author', value='[{owner}](https://mods.factorio.com/user/{owner})'.format(owner=json_req['owner']))
+            embed = discord.Embed(title='Mod not found', description='Failed to find mods', color=discord.Color.from_rgb(255, 10, 10))
             await ctx.send(embed=embed)
-        else:
-            new_mod_name = self.find_mod(mod_name)
-            if new_mod_name:
-                await ctx.invoke(self.new_mods_statistics, mod_name=new_mod_name)
-            else:
-                embed = discord.Embed(title='Mod not found', description='Failed to find mod \'{}\''.format(mod_name), color=discord.Color.from_rgb(255, 10, 10))
-                await ctx.send(embed=embed)
+
+    async def send_mod_embed(self, ctx: commands.Context, mod_data: dict):
+        embed = discord.Embed(title=mod_data['title'], description=mod_data['description'], url=mod_data['url'], timestamp=mod_data['timestamp'], color=mod_data['color'])
+        embed.set_footer(text='Latest update was released at:')
+        if mod_data['thumbnail_url']:
+            embed.set_thumbnail(url=mod_data['thumbnail_url'])
+        embed.add_field(name='Game version', value=mod_data['game_version'])
+        embed.add_field(name='Download', value='[From official mod portal]({})\n[From Factorio Launcher storage]({})'.format(mod_data['download_official'], mod_data['download_launcher']))
+        embed.add_field(name='Latest version', value=mod_data['latest_version'])
+        embed.add_field(name='Downloaded', value=str(mod_data['downloads_count'])+' times')
+        embed.add_field(name='Author', value='[{author}](https://mods.factorio.com/user/{author})'.format(author=mod_data['author']))
+        await ctx.send(embed=embed)
 
     async def get_mods_info(self, mod_names: list) -> list:
         result = []
@@ -83,13 +60,13 @@ class FactorioCog(commands.Cog, name='Factorio'):
                     download_official='https://mods.factorio.com'+latest_release['download_url'], download_launcher='https://factorio-launcher-mods.storage.googleapis.com/{}/{}.zip'.format(
                         mod_name, latest_release['version']), latest_version=latest_release['version'], downloads_count=json_req['downloads_count'], author=json_req['owner']))
             else:
-                new_mod_name = self.find_mod(mod_name)
+                new_mod_name = await self.find_mod(mod_name)
                 if new_mod_name:
-                    result.append(*self.get_mods_info([new_mod_name]))
+                    result.append(*await self.get_mods_info([new_mod_name]))
         return result
 
     @staticmethod
-    def find_mod(mod_name: str) -> str:
+    async def find_mod(mod_name: str) -> str:
         request = req.get('https://mods.factorio.com/query/'+mod_name.replace(' ', '%20'))
         if request.status_code == 200:
             soup = BeautifulSoup(request.text, 'html.parser')
@@ -104,9 +81,7 @@ class FactorioCog(commands.Cog, name='Factorio'):
 
     @commands.command()
     async def modlist(self, ctx: commands.Context):
-        for mod_name in MOD_LIST_MOTORCHIK:
-            await ctx.invoke(self.new_mods_statistics, mod_name=mod_name)
-        await ctx.message.delete()
+        await ctx.invoke(self.newer_mods_statistics, mods_names=MOD_LIST_MOTORCHIK)
 
 
 def setup(bot: commands.Bot):
