881e85492548185b6ee73898087f2fa3e01199da
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-11-28 23:53:04 +0300

    Improved codestyle

--- moderation.py
+++ moderation.py
@@ -7,8 +7,8 @@ class Moderation(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(aliases=['clear', 'c'], description='Clear chat', brief='Clear chat', help='Deletes specified count of messages in this channel. Can be used only by members with messages managing permission.')
     @commands.has_permissions(manage_messages=True)
+    @commands.command(aliases=['clear', 'c'], description='Clear chat', brief='Clear chat', help='Deletes specified count of messages in this channel. Can be used only by members with messages managing permission.')
     async def clearchat(self, ctx: commands.Context, messages_count: int):
         await ctx.message.delete()
         deleted = await ctx.channel.purge(limit=messages_count)
