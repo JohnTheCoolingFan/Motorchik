0118941e99b02672cd951249e82d54e0826299cf
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-12-16 19:59:33 +0300

    GuildXp is a valid Cog now...

--- guild_xp.py
+++ guild_xp.py
@@ -6,7 +6,11 @@ class GuildXp(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.guild and message.guild.id == 370167294439063564:
             guildconfig = GuildConfig(message.guild)
             guildconfig.process_message(message)
+
+def setup(bot: commands.Bot):
+    bot.add_cog(GuildXp(bot))
