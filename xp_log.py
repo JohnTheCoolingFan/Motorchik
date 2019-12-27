36fe6eb96c5e4159df9737725cffe607fa8a4c80
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-12-27 16:14:59 +0300

    Remember current time on disconnect

--- xp_log.py
+++ xp_log.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from guild_config import GuildConfig
+import datetime
 
 class XpLogCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -11,6 +12,11 @@ class XpLogCog(commands.Cog):
         if message.guild and message.guild.id == 370167294439063564: # Temporary restriction because still testing
             guildconfig = GuildConfig(message.guild)
             guildconfig.process_message(message)
+
+    @commands.Cog.listener()
+    async def on_disconnect(self):
+        with open('latest_connection.txt', 'w') as timestamp_file:
+            timestamp_file.write(datetime.datetime.now().timestamp())
 
 def setup(bot: commands.Bot):
     bot.add_cog(XpLogCog(bot))
