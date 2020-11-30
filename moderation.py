937a9b88b6d2bac45e37f6658ca3053558a0c8dd
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-11-30 19:59:28 +0300

    Added unban and kick commands

--- moderation.py
+++ moderation.py
@@ -6,8 +6,6 @@ from guild_config import GuildConfig
 class Moderation(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-    # TODO: add on_member_ban listener
-    # TODO: move these listeners to InfoChannels
 
     @commands.has_permissions(manage_messages=True)
     @commands.command(aliases=['clear', 'cl'], description='Clear chat', brief='Clear chat', help='Deletes specified count of messages in this channel. Can be used only by members with messages managing permission.')
@@ -24,6 +22,24 @@ class Moderation(commands.Cog):
         await ctx.send('Banned member '+str(member))
         if guild_config.log_channel:
             await guild_config.log_channel.send('Banned {0}\nReason: {1}'.format(str(member), reason))
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command(case_sensitive=False, description='Unban user', help='Unban specified member.')
+    async def unban(self, ctx: commands.Context, member: discord.Member, reason: str = 'Not provided'):
+        guild_config = GuildConfig(ctx.guild)
+        await member.unban(reason=reason)
+        await ctx.send('Unbanned member '+str(member))
+        if guild_config.log_channel:
+            await guild_config.log_channel.send('Unbanned {0}\nReason: {1}'.format(str(member), reason))
+
+    @commands.has_permissions(kick_members=True)
+    @commands.command(case_sensitive=False, description='Kick member', help='Kick specified member')
+    async def kick(self, ctx: commands.Context, member: discord.Member, reason: str = 'Not provided'):
+        guild_config = GuildConfig(ctx.guild)
+        await member.kick(reason=reason)
+        await ctx.send('Kicked member '+str(member))
+        if guild_config.log_channel:
+            await guild_config.log_channel.send('Kicked {0}\nReason: {1}'.format(str(member), reason))
 
 
 def setup(bot: commands.Bot):
