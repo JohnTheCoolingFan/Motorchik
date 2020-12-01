eee389d9a7aba0ab359cad7051f6c9ce2e21a879
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-12-02 00:06:10 +0300

    BotConfig Cog functionality

--- bot_config.py
+++ bot_config.py
@@ -2,15 +2,17 @@ import json
 import discord
 from discord.ext import commands
 
-class BotConfig:
+class BotConfig(commands.Cog):
     bot: commands.Bot
+    log_channel: discord.TextChannel
     token: str
 
     def __init__(self, filename, bot: commands.Bot):
         with open(filename, 'r') as bot_config_file:
             self.raw = json.load(bot_config_file)
         self.bot = bot
+        self.log_channel = bot.get_channel(self.raw['log_channel'])
+        self.token = self.raw['token']
 
-    @property
-    def log_channel(self) -> discord.TextChannel:
-        return self.bot.get_channel(self.raw['log_channel'])
+def setup(bot: commands.Bot):
+    bot.add_cog(BotConfig('config.json', bot))
