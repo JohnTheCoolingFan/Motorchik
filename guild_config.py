eaefd5f25f9c015fa528e903b309651b681f4f33
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-03-20 01:41:26 +0300

    Added add_guild method

--- guild_config.py
+++ guild_config.py
@@ -158,6 +158,28 @@ class GuildConfigCog(commands.Cog):
     async def get_config(self, guild: discord.Guild) -> GuildConfig:
         pass
 
+    async def add_guild(self, guild: discord.Guild):
+        default_channel = guild.system_channel.id if guild.system_channel is not None else guild.text_channels[0].id
+        # New entries may be added in the future.
+        guild_config_data = {
+            "guild_id": guild.id,
+            "guild_name": guild.name, # For debugging purposes.
+            "default_roles": [], # Empty list by default
+            "info_channels": { # Names are hardcoded but there is some clearance for expanding Info Channels functionality
+                "welcome": {
+                    "channel_id": default_channel, # Use default system channel or the first text channel
+                    "enabled": False # Disabled by default
+                },
+                "log": {
+                    "channel_id": default_channel, # Same as with 'welcome' info channel
+                    "enabled": False # Disabled by default
+                }
+            },
+            "command_filters": [] # Empty list that will be filled when setting up
+        }
+        guilds_collection = self.mongo_db.guilds
+        guilds_collection.insert_one(guild_config_data)
+
 class MotorchikBot(commands.Bot):
     async def guild_config(self, guild: discord.Guild) -> GuildConfig:
         return self.get_cog('GuildConfigCog').get_config(guild)
