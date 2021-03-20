856fc867574fcf22cdbf6b4382b60bff180055f1
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-03-20 15:09:37 +0300

    GuildConfig rewrite part 1
    Getters stay nearly the same.
    Removed many classmethods. Also json dumping can be moved to the cog.
    XP system is getting removed from everywhere to be re-implemented later.
    Setters are still TODO

--- guild_config.py
+++ guild_config.py
@@ -31,21 +31,20 @@ class CommandDisabledException(discord.DiscordException):
         self.channel = channel
         self.disability = disability
 
+# TODO: updating data
 class GuildConfig:
     guild: discord.Guild
-    raw: dict
-    commands_names: List[str]
 
-    def __init__(self, guild: discord.Guild):
+    def __init__(self, guild: discord.Guild, guild_config_data: pymongo.cursor.Cursor):
+        # The guild_config_data type is weird but that's what is being returned by pymongo's find_one() (probably).
+        # But it can still be used as a dict, I think.
         self.guild = guild
-        with open('guilds/guild_{}.json'.format(guild.id), 'r') as guild_config_file:
-            self.raw = json.load(guild_config_file)
-        self.commands_names = self.raw['commands'].keys()
+        self.raw_data = guild_config_data
 
     @property
     def welcome_channel(self) -> Optional[discord.TextChannel]:
-        if self.raw['info_channels']['welcome']['enabled']:
-            return self.guild.get_channel(self.raw['info_channels']['welcome']['channel_id'])
+        if self.raw_data['info_channels']['welcome']['enabled']:
+            return self.guild.get_channel(self.raw_data['info_channels']['welcome']['channel_id'])
         else:
             return None
 
@@ -55,8 +54,8 @@ class GuildConfig:
 
     @property
     def log_channel(self) -> Optional[discord.TextChannel]:
-        if self.raw['info_channels']['log']['enabled']:
-            return self.guild.get_channel(self.raw['info_channels']['log']['channel_id'])
+        if self.raw_data['info_channels']['log']['enabled']:
+            return self.guild.get_channel(self.raw_data['info_channels']['log']['channel_id'])
         else:
             return None
 
@@ -66,87 +65,68 @@ class GuildConfig:
 
     @property
     def default_roles(self) -> List[discord.Role]:
-        return [self.guild.get_role(role_id) for role_id in self.raw['default_roles']]
+        return [self.guild.get_role(role_id) for role_id in self.raw_data['default_roles']]
 
     @default_roles.setter
     def default_roles(self, new_roles: Iterable[discord.Role]):
-        self.raw['default_roles'] = list({role.id for role in new_roles})
+        self.raw_data['default_roles'] = list({role.id for role in new_roles})
 
-    @classmethod
-    async def check(cls, bot: commands.Bot, guild: discord.Guild = None):
-        def check_for_entry(entry: str, guild_config_to_check: cls, entry_type):
-            if entry not in guild_config_to_check.raw:
-                guild_config_to_check.raw[entry] = entry_type()
-        if guild is None:
-            await asyncio.wait([cls.check(bot, _guild) for _guild in bot.guilds])
-        else:
-            if not os.path.exists('guilds/guild_{}.json'.format(guild.id)):
-                cls.create_guild_config(guild)
-            guild_config = cls(guild)
-            print('Checking config of guild ID:{}'.format(guild.id))
-            for entry_name in GUILD_CONFIG_ENTRIES_TYPES:
-                check_for_entry(entry_name, guild_config, GUILD_CONFIG_ENTRIES_TYPES[entry_name])
-            for command in bot.commands:
-                if command.name not in guild_config.raw['commands'].keys():
-                    print('Config for command "{0}" not found in config of guild ID:{1}'.format(command.name, guild_config.guild.id))
-                    guild_config.raw['commands'][command.name] = dict(whitelist=[], blacklist=[], enabled=True)
-            guild_config.write()
+# May be reused somewhere
+#    @classmethod
+#    async def check(cls, bot: commands.Bot, guild: discord.Guild = None):
+#        def check_for_entry(entry: str, guild_config_to_check: cls, entry_type):
+#            if entry not in guild_config_to_check.raw:
+#                guild_config_to_check.raw[entry] = entry_type()
+#        if guild is None:
+#            await asyncio.wait([cls.check(bot, _guild) for _guild in bot.guilds])
+#        else:
+#            if not os.path.exists('guilds/guild_{}.json'.format(guild.id)):
+#                cls.create_guild_config(guild)
+#            guild_config = cls(guild)
+#            print('Checking config of guild ID:{}'.format(guild.id))
+#            for entry_name in GUILD_CONFIG_ENTRIES_TYPES:
+#                check_for_entry(entry_name, guild_config, GUILD_CONFIG_ENTRIES_TYPES[entry_name])
+#            for command in bot.commands:
+#                if command.name not in guild_config.raw['commands'].keys():
+#                    print('Config for command "{0}" not found in config of guild ID:{1}'.format(command.name, guild_config.guild.id))
+#                    guild_config.raw['commands'][command.name] = dict(whitelist=[], blacklist=[], enabled=True)
+#            guild_config.write()
 
-    def add_xp(self, member: discord.Member, xp_count: int):
-        if str(member.id) not in self.raw['members']:
-            self.raw['members'][str(member.id)] = 0
-        self.raw['members'][str(member.id)] += xp_count
-        self.write()
+# Unimplemented for now
+#    def add_xp(self, member: discord.Member, xp_count: int):
+#        if str(member.id) not in self.raw['members']:
+#            self.raw['members'][str(member.id)] = 0
+#        self.raw['members'][str(member.id)] += xp_count
+#        self.write()
+#
+#    def get_xp(self, member: discord.Member) -> int:
+#        return self.raw['members'][str(member.id)]
 
-    def get_xp(self, member: discord.Member) -> int:
-        return self.raw['members'][str(member.id)]
+# Don't work with command filters
+#    def switch_command(self, command_name: str, new_state: bool) -> bool:
+#        if command_name in self.commands_names:
+#            self.raw['commands'][command_name]['enabled'] = new_state
+#            self.write()
+#            return True
+#        else:
+#            return False
+#
+#    def set_command_filter(self, command_name: str, filter_name: str, new_filter: Iterable[discord.TextChannel]) -> bool:
+#        if command_name in self.commands_names:
+#            self.raw['commands'][command_name][filter_name] = list({channel.id for channel in new_filter})
+#            self.write()
+#            return True
+#        else:
+#            return False
 
-    @staticmethod
-    def create_guild_config(guild: discord.Guild):
-        print('Creating guild config file for ID:{}'.format(guild.id))
-        with open('guilds/guild_{}.json'.format(guild.id), 'w') as new_guild_config_file:
-            default_channel = guild.system_channel.id if guild.system_channel is not None else guild.text_channels[0].id
-            default_info_channel = dict(channel_id=default_channel, enabled=False)
-            new_guild_config = dict(name=guild.name,
-                                    default_roles=[],
-                                    commands=dict(),
-                                    members=dict(),
-                                    levels=dict(),
-                                    info_channels=dict(log=default_info_channel,
-                                                       welcome=default_info_channel))
-            json.dump(new_guild_config, new_guild_config_file, sort_keys=True, indent=4)
-
-    def write(self):
-        with open('guilds/guild_{}.json'.format(self.guild.id), 'w') as guild_config:
-            json.dump(self.raw, guild_config, indent=4, sort_keys=True)
-
-    def switch_command(self, command_name: str, new_state: bool) -> bool:
-        if command_name in self.commands_names:
-            self.raw['commands'][command_name]['enabled'] = new_state
-            self.write()
-            return True
-        else:
-            return False
-
-    def set_command_filter(self, command_name: str, filter_name: str, new_filter: Iterable[discord.TextChannel]) -> bool:
-        if command_name in self.commands_names:
-            self.raw['commands'][command_name][filter_name] = list({channel.id for channel in new_filter})
-            self.write()
-            return True
-        else:
-            return False
-
-    def switch_info_channel(self, info_channel_type: str, new_state: bool) -> bool:
-        if info_channel_type in ['welcome', 'log', 'reports']:
-            self.raw['info_channels'][info_channel_type]['enabled'] = new_state
-            self.write()
-            return True
-        else:
-            return False
-
-    @property
-    def json(self) -> str:
-        return json.dumps(self.raw, sort_keys=True, indent=4)
+# TODO
+#    def switch_info_channel(self, info_channel_type: str, new_state: bool) -> bool:
+#        if info_channel_type in ['welcome', 'log']:
+#            self.raw_data['info_channels'][info_channel_type]['enabled'] = new_state
+#            self.write()
+#            return True
+#        else:
+#            return False
 
 class GuildConfigCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -157,7 +137,7 @@ class GuildConfigCog(commands.Cog):
     async def get_config(self, guild: discord.Guild) -> GuildConfig:
         guilds_collection = self.mongo_db.guilds
         guild_config_data = guilds_collection.find_one({"guild_id": guild.id})
-        return GuildConfig(guild_config_data)
+        return GuildConfig(guild, guild_config_data)
 
     async def add_guild(self, guild: discord.Guild):
         default_channel = guild.system_channel.id if guild.system_channel is not None else guild.text_channels[0].id
@@ -183,4 +163,4 @@ class GuildConfigCog(commands.Cog):
 
 class MotorchikBot(commands.Bot):
     async def guild_config(self, guild: discord.Guild) -> GuildConfig:
-        return self.get_cog('GuildConfigCog').get_config(guild)
+        return await self.get_cog('GuildConfigCog').get_config(guild)
