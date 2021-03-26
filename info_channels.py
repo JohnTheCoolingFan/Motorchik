8969f2ebe595d11a2fc4823ea1d4483076280bdd
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-03-26 19:03:58 +0300

    GuildConfig cog in InfoChannels

--- info_channels.py
+++ info_channels.py
@@ -1,21 +1,21 @@
 from discord.ext import commands
 import discord
 from typing import Union
-from guild_config import GuildConfig
 
 class InfoChannels(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.guild_config_cog = bot.get_cog('GuildConfigCog')
 
     @commands.Cog.listener()
     async def on_member_ban(self, guild: discord.Guild, user: Union[discord.User, discord.Member]):
-        guild_config = GuildConfig(guild)
+        guild_config = await self.guild_config_cog.get_guild(guild)
         if guild_config.log_channel:
             await guild_config.log_channel.send('{} was banned'.format(str(user)))
 
     @commands.Cog.listener()
     async def on_member_unban(self, guild: discord.Guild, user: discord.User):
-        guild_config = GuildConfig(guild)
+        guild_config = await self.guild_config_cog.get_guild(guild)
         if guild_config.log_channel:
             await guild_config.log_channel.send('{} was unbanned'.format(str(user)))
 
