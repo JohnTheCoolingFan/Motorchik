8932f5b99e03580f060f286cee02b1b1d6e02ac9
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-04-03 22:57:24 +0300

    Added authSource

--- mongo_client.py
+++ mongo_client.py
@@ -18,7 +18,8 @@ class GuildConfigCog(commands.Cog):
                                                bot_config.mongo['port'],
                                                username=bot_config.mongo['username'] if 'username' in bot_config.mongo else None,
                                                password=bot_config.mongo['password'] if 'password' in bot_config.mongo else None,
-                                               io_loop=bot.loop)
+                                               io_loop=bot.loop,
+                                               authSource='motorchik_guild_config')
         self.bot = bot
         self.mongo_db = self.mongo_client['motorchik_guild_config']
         self.guilds_collection = self.mongo_db.guilds
