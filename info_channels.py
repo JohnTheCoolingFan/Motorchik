e2992ed54302bd8ba0960a73318388c7d68d0f4c
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-04-09 15:22:18 +0300

    Fix properties

--- info_channels.py
+++ info_channels.py
@@ -1,6 +1,8 @@
-from discord.ext import commands
-import discord
 from typing import Union
+
+import discord
+from discord.ext import commands
+
 
 class InfoChannels(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -10,14 +12,14 @@ class InfoChannels(commands.Cog):
     @commands.Cog.listener()
     async def on_member_ban(self, guild: discord.Guild, user: Union[discord.User, discord.Member]):
         guild_config = await self.guild_config_cog.get_guild(guild)
-        if guild_config.log_channel:
-            await guild_config.log_channel.send('{} was banned'.format(str(user)))
+        if guild_config.get_log_channel():
+            await guild_config.get_log_channel().send('{} was banned'.format(str(user)))
 
     @commands.Cog.listener()
     async def on_member_unban(self, guild: discord.Guild, user: discord.User):
         guild_config = await self.guild_config_cog.get_guild(guild)
-        if guild_config.log_channel:
-            await guild_config.log_channel.send('{} was unbanned'.format(str(user)))
+        if guild_config.get_log_channel():
+            await guild_config.get_log_channel().send('{} was unbanned'.format(str(user)))
 
 def setup(bot: commands.Bot):
     bot.add_cog(InfoChannels(bot))
