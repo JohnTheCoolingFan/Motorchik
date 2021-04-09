08c0c085894e04e1d347a0babab561d54e27f63a
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-04-09 15:21:21 +0300

    Fixed properties

--- greetings.py
+++ greetings.py
@@ -1,7 +1,8 @@
-from discord.ext import commands, tasks
-import discord
-from typing import List
 from datetime import datetime, timedelta
+from typing import List
+
+import discord
+from discord.ext import commands, tasks
 
 
 class QueueItem:
@@ -22,20 +23,20 @@ class Greetings(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         guild_config = await self.guild_config_cog.get_guild(member.guild)
-        if guild_config.welcome_channel:
-            await guild_config.welcome_channel.send('Welcome, {0.mention}'.format(member))
-        if guild_config.default_roles:
+        if guild_config.get_welcome_channel():
+            await guild_config.get_welcome_channel().send('Welcome, {0.mention}'.format(member))
+        if guild_config.get_default_roles():
             if member.guild.verification_level != discord.VerificationLevel.none:
                 print("Putting user {} on queue".format(member.id))
-                self.queue.append(QueueItem(member, guild_config.default_roles))
+                self.queue.append(QueueItem(member, guild_config.get_default_roles()))
             else:
-                await member.add_roles(*guild_config.default_roles, reason='New member join.')
+                await member.add_roles(*guild_config.get_default_roles(), reason='New member join.')
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
         guild_config = await self.guild_config_cog.get_guild(member.guild)
-        if guild_config.welcome_channel:
-            await guild_config.welcome_channel.send('Goodbye, {0} (ID:{1})'.format(str(member), member.id))
+        if guild_config.get_welcome_channel():
+            await guild_config.get_welcome_channel().send('Goodbye, {0} (ID:{1})'.format(str(member), member.id))
 
     @tasks.loop(seconds=10)
     async def queue_checker(self):
