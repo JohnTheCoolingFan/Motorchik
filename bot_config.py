916612218fa522f9428cbd340991697e13bc1a51
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-05-05 15:37:58 +0300

    Different config storage methods
    Added mongo_init and json_init, added config storage method

--- bot_config.py
+++ bot_config.py
@@ -1,6 +1,7 @@
 import json
 import os.path as p
 import sys
+from typing import Optional
 
 import discord
 from discord.ext import commands
@@ -10,7 +11,9 @@ class BotConfig(commands.Cog):
     bot: commands.Bot
     log_channel: discord.TextChannel
     token: str
-    mongo: dict
+    storage_method: str
+    mongo: Optional[dict]
+    json: Optional[dict]
 
     def __init__(self, filename, bot: commands.Bot):
         print('Loading BotConfig module...', end='')
@@ -20,22 +23,36 @@ class BotConfig(commands.Cog):
             self.bot = bot
             self.log_channel = bot.get_channel(self.raw['log_channel'])
             self.token = self.raw['token']
-            self.mongo = dict()
-            self.mongo['host'] = self.raw['mongo']['host']
-            port = self.raw['mongo']['port']
-            if port:
-                try:
-                    if port is not int:
-                        port = int(port)
-                    self.mongo['port'] = port
-                except ValueError:
-                    print('Invalid port: {}'.format(port))
-            self.mongo['username'] = self.raw['mongo']['username']
-            self.mongo['password'] = self.raw['mongo']['password']
+            if self.raw['storage_method'] == 'mongo':
+                if 'mongo' in self.raw:
+                    self.mongo_init()
+                else:
+                    print('Error: storage_method is "mongo", but there is no configuration for MongoDB (no "mongo" entry)')
+                    sys.exit(1)
             print(' Done')
         else:
             print('Error: config.json does not exist')
             sys.exit(1)
+
+    def mongo_init(self):
+        self.storage_method = 'mongo'
+        self.mongo = dict()
+        self.mongo['host'] = self.raw['mongo']['host']
+        port = self.raw['mongo']['port']
+        if port:
+            try:
+                if port is not int:
+                    port = int(port)
+                self.mongo['port'] = port
+            except ValueError:
+                print('Invalid port: {}'.format(port))
+        self.mongo['username'] = self.raw['mongo']['username']
+        self.mongo['password'] = self.raw['mongo']['password']
+
+    def json_init(self):
+        self.storage_method = 'mongo'
+        self.json = dict()
+        self.json['dir'] = 'guilds' if 'dir' not in self.raw['json'] else self.raw['json']['dir'] # Set the dir
 
 def setup(bot: commands.Bot):
     bot.add_cog(BotConfig('config.json', bot))
